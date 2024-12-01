fec647dd169baf13618d4edc7f8ad7bd8ec6f7d7
Helen Diana <diana_helen_14@hotmail.com>
2024-11-30 22:16:04 -0500

    Add tests

--- msaccount/src/test/java/com/api/msaccount/MsaccountApplicationTests.java
+++ msaccount/src/test/java/com/api/msaccount/MsaccountApplicationTests.java
@@ -1,13 +1,58 @@
 package com.api.msaccount;
 
+
+import com.api.msaccount.enums.AccountType;
+import com.api.msaccount.model.Account;
+import com.api.msaccount.service.impl.AccountServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 class MsaccountApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    /*@Test
+    void contextLoads() {
+    }*/
+    
+    @Autowired
+    private AccountServiceImpl accountServiceImpl;
+    
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);       
+    }
+
+    @Test
+    public void testObtenerAccountPorId() {
+        Optional<Account> account = accountServiceImpl.getAccountById(1);
+        assertTrue(account.isPresent());
+    }
+
+    @Test
+    public void testAllAccounts() {
+        List<Account> accounts = accountServiceImpl.listAccounts();
+        assertNotNull(accounts);
+    }
+    
+    @Test
+    public void testGuardarAccount() {
+        Account _account = new Account();
+        _account.setAccountType(AccountType.CORRIENTE);
+        _account.setBalance(3800);
+        _account.setId((long)1123);        
+        Account accountSave = accountServiceImpl.saveAccount(_account);
+        assertNotNull(accountSave);
+    }
 
 }
--- mscustomer/src/test/java/com/api/mscustomer/MscustomerApplicationTests.java
+++ mscustomer/src/test/java/com/api/mscustomer/MscustomerApplicationTests.java
@@ -1,13 +1,55 @@
 package com.api.mscustomer;
 
+import com.api.mscustomer.model.Customer;
+import com.api.mscustomer.service.impl.CustomerServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 class MscustomerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private CustomerServiceImpl customerServiceImpl;
 
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);       
+    }
+
+    @Test
+    public void testObtenerCustomerPorId() {
+        Optional<Customer> customer = customerServiceImpl.getCustomerById(2);
+        assertTrue(customer.isPresent());
+        assertEquals("Roberto", customer.get().getFirstName());
+    }
+
+    @Test
+    public void testAllCustomers() {
+        List<Customer> customers = customerServiceImpl.listCustomers();
+        assertNotNull(customers);
+        //assertEquals(3, customers.size());
+        assertEquals("Marcos", customers.get(0).getFirstName());
+    }
+    
+    @Test
+    public void testGuardarCustomer() {
+        Customer _customer = new Customer();
+        _customer.setDocument("123456");
+        _customer.setEmail("test@gmail.com");
+        _customer.setFirstName("Test");
+        _customer.setLastName("Test2");
+        Customer customerSave = customerServiceImpl.saveCustomer(_customer);
+        assertNotNull(customerSave);
+        assertEquals("Test", customerSave.getFirstName());
+    }
 }
